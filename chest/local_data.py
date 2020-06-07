a16af9c78e1fa0b78066139d06b399b0102a0f6f
quantumsheep <nathanael.dmc@outlook.fr>
2020-06-07 16:52:19 +0200

    Add local storage system

---
+++ chest/local_data.py
@@ -0,0 +1,35 @@
+import sys
+import pathlib
+
+
+def __get_datadir() -> pathlib.Path:
+    home = pathlib.Path.home()
+
+    if sys.platform == "win32":
+        return home / "AppData/Roaming"
+    elif sys.platform.startswith('linux'):
+        return home / ".local/share"
+    elif sys.platform == "darwin":
+        return home / "Library/Application Support"
+
+
+def appdir(path: str = '') -> pathlib.Path:
+    if not path.startswith('/'):
+        path = '/' + path
+
+    dirpath = __get_datadir() / ("chest" + path)
+
+    dirpath.mkdir(parents=True, exist_ok=True)
+
+    return dirpath
+
+
+def appfile(filename: str, path: str = '') -> pathlib.Path:
+    if filename.startswith('/'):
+        filename = filename[1::]
+
+    filepath = appdir(path) / filename
+
+    filepath.touch(exist_ok=True)
+
+    return filepath
