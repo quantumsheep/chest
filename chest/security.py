f3cc36f167926863bc97c82e9df9426a107dd819
quantumsheep <nathanael.dmc@outlook.fr>
2020-06-07 18:46:56 +0200

    Add encryption and decryption functions (using AES-256 GCM)

--- chest/security.py
+++ chest/security.py
@@ -1,5 +1,8 @@
+import os
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 
 def hash(data: bytes) -> bytes:
@@ -7,3 +10,25 @@ def hash(data: bytes) -> bytes:
     digest.update(data)
 
     return digest.finalize()
+
+
+def hash_str(data: str) -> bytes:
+    return hash(bytes(data, 'utf-8'))
+
+
+def encrypt(data: bytes, password: str) -> bytes:
+    aesgcm = AESGCM(hash_str(password))
+    nonce = os.urandom(12)
+
+    ct = aesgcm.encrypt(nonce,  data, None)
+
+    return nonce + ct
+
+
+def decrypt(data: bytes, password: str) -> bytes:
+    aesgcm = AESGCM(hash_str(password))
+    nonce = data[0:12]
+
+    ct = aesgcm.decrypt(nonce, data[12::], None)
+
+    return ct
